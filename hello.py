3850d05d7092e4c54eec96f6db07ce7dc03f6f2f
jieyanqiang <jieyanqiang@lf-network.com>
2022-10-26 20:17:49 +0800

    feat: test

---
+++ hello.py
@@ -0,0 +1,24 @@
+#!/user/bin/python
+# -*- coding: utf-8 -*-
+# Python 中文编码
+
+import time
+
+# 打印
+print ("hello, world!")
+print ("您好，世界")
+
+# 循环
+for i in range(0,10):
+	print ("i=", i)
+
+# 时间
+time = time.time()
+print ("time=", time)
+
+# 函数
+def printStr(str):
+	print str
+	return
+
+printStr("abc")	
