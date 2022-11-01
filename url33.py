ef46eb9271cccf0ef97518fdda4a90531d6b0438
jieyanqiang <jieyanqiang@lf-network.com>
2022-11-02 00:42:49 +0800

    feat: add log

--- url33.py
+++ url33.py
@@ -12,15 +12,12 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
 # 目标站点
 urlHost = 'www.cnipa.gov.cn'
-# urlTarget = 'https://www.cnipa.gov.cn/'
 
 # 目标url
 # urlTarget = 'https://' + urlHost + '/'
 urlTarget = 'https://' + urlHost
-
 
 # 存放还没访问的url
 urlSetNotVisit = set()
@@ -37,6 +34,35 @@ urlMapParent = {}
 # 访问页面计数器
 pageVisitedNum = 0
 
+
+# 获取字符串格式的html_doc。由于content为bytes类型，故需要decode()
+# html_doc = requests.get('https://xkcd.com/353/').content.decode()
+# 加 headers
+# headers = {'user-agent': 'my-app/0.0.1'}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0'}
+
+
+# 初始目标url
+# urlTarget = 'https://www.gov.cn/zhuanti/zggcddescqgdbdh/index.htm'
+# pdf
+# urlTarget = 'https://www.cnipa.gov.cn/module/download/down.jsp?i_ID=176955&colID=74'
+# urlTarget = 'https://www.cnipa.gov.cn/attach/0/9607103da7fa4ae493f000bd0303f565.pdf'
+# title IndexError: list index out of range
+# urlTarget = 'https://pss-system.cponline.cnipa.gov.cn'
+# urlTarget = 'http://wsgs.sbj.cnipa.gov.cn:9080/tmpu/'
+
+targetUrlSeedSet = {urlTarget}
+
+
+# 默认状态 0
+serverResponseStatusCode = 0
+
+# 日志文件
+csvFileName = './storage/link_' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + '.csv'
+logFileName = './storage/log_' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + '.log'
+
+
 # 获取状态码
 
 
@@ -52,15 +78,16 @@ def getHttpStatusCode(url):
 
 
 def getHttpRequest(url):
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0'}
+    global headers
+
     try:
         return requests.get(url, headers=headers, verify=False)
     except requests.exceptions.HTTPError as e:
         return e
 
-
 # 检测字符串是否url
+
+
 def checkIsUrl(strx):
     return strx.startswith("http")
 
@@ -74,161 +101,130 @@ def returnUrl(strx):
         return 'https://www.cnipa.gov.cn/' + strx
 
 
-# 获取字符串格式的html_doc。由于content为bytes类型，故需要decode()
-# html_doc = requests.get('https://xkcd.com/353/').content.decode()
-# 加 headers
-# headers = {'user-agent': 'my-app/0.0.1'}
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0'}
+with open(logFileName, 'w') as logFile:
+    # 写csv
+    with open(csvFileName, 'w', newline='') as csvFile:
+        fieldnames = ['link', 'text', 'pageWhereFound', 'serverResponse']
+        writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        writer.writeheader()
 
-# html_doc = requests.get(url, headers=headers).content.decode()
-# urlRequest = requests.get(urlTarget, headers=headers, verify=False)
+        while (targetUrlSeedSet):
+            for urlPath in targetUrlSeedSet:
+                logContent = ('\n %s: 正在访问第 %s 个链接, urlPath:\n %s' %
+                              (
+                                  time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()),
+                                  pageVisitedNum,
+                                  urlPath
+                              ))
 
-''' htmlDoc = getHttpRequest(urlTarget).content.decode()
-# 使用BeautifulSoup模块对页面文件进行解析
-soup = BeautifulSoup(htmlDoc, 'html.parser')
-# print("soup==")
-# print(soup)
+                print(logContent)
 
-# 查找所有tag为'a'的html元素，并生成列表
-# links = soup.find_all('a')
-links = soup.find_all('a')
+                # 写日志
+                logFile.write(logContent)
+                logFile.flush()
 
-# links = soup.find_all('a target')
-print("links==")
-print(links)
+                # urlPath 转 完整url
+                urlFull = returnUrl(urlPath)
+                # print(urlPath, urlFull)
+                # exit()
 
-# time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-fileName = './storage/link_' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + '.csv'
-with open(fileName, 'w', newline='') as csvfile:
-    fieldnames = ['link', 'text', 'pageWhereFound', 'serverResponse']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for item in links:
-        print("content==")
-        print(item.contents)
-        # print(''.join(item.contents))
-        # 列表转字符串
-        contentList = map(lambda x: str(x), item.contents)
-        contentStr = ''.join(contentList)
-        print(contentStr)
+                # 下载页面
+                print('%s: 正在访问第 %s 个链接, urlFull:\n %s' %
+                      (time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()), pageVisitedNum, urlFull))
+                pageVisitedNum += 1
 
-        urlHref = item.get('href')
-        print("url==")
-        print(urlHref)
-        # 写入 csv
-        pageWhereFound = ''
-        serverResponseStatusCode = getHttpStatusCode(returnUrl(urlHref))
-        writer.writerow(
-            {
-                'link': urlHref,
-                'text': contentStr,
-                'pageWhereFound': pageWhereFound,
-                'serverResponse': serverResponseStatusCode
-            }
-        ) '''
+                try:
+                    page = getHttpRequest(urlFull)
+                except requests.exceptions.HTTPError as e:
+                    # 写日志
+                    logFile.write(('\n异常request urlFull: %s, code: %s, message: %s' %
+                                  (urlFull, str(e.code), e.message)))
+                    logFile.flush()
+                    continue
+                    pass
+                # page = getHttpRequest(urlFull)
+                # print(page.get("Content-Type"))
+                # exit()
 
-# todo 去重
-# todo 递归
-# urlTarget = 'https://www.gov.cn/zhuanti/zggcddescqgdbdh/index.htm'
-targetUrlSeedSet = {urlTarget}
-serverResponseStatusCode = 0
+                    # TODO 只解析 utf8
+                try:
+                    htmlDoc = page.content.decode()
+                except Exception as e:
+                    # 写日志
+                    logFile.write(('\n异常解码 urlFull: %s, code: %s, message: %s' % (urlFull, '', '')))
+                    logFile.flush()
 
-# 日志文件
-fileName = './storage/link_' + time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + '.csv'
+                    continue
+                    pass
 
-# 写csv
-with open(fileName, 'w', newline='') as csvfile:
-    fieldnames = ['link', 'text', 'pageWhereFound', 'serverResponse']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+                # 解析网页
+                soup = BeautifulSoup(htmlDoc, 'html.parser')
+                # print(htmlDoc)
+                # exit()
 
-    while (targetUrlSeedSet):
-        for urlPath in targetUrlSeedSet:
-            print('%s: 正在访问第 %s 个链接, urlPath:\n %s' %
-                  (time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()), pageVisitedNum, urlPath))
+                # 获取标题
+                # titles = soup.findall("<title>(.+?)</title>", str(htmlDoc))
+                # titles = soup.find_all('a')
+                titles = soup.find_all("title")
+                # links = soup.find_all('a')
 
-            # print(urlPath)
+                # print(titles[0], titles[0].contents, titles[0].contents[0])
+                # print(titles, titles[0].contents)
+                # exit()
+
+                if titles and titles[0] and titles[0].contents:
+                    title = titles[0].contents[0]
+                else:
+                    title = '页面标题为空'
+
+                # 写入 csv
+                pageWhereFound = ''
+                writer.writerow(
+                    {
+                        'link': urlFull,
+                        'text': title,
+                        'pageWhereFound': str(urlMapParent.get(urlFull)),
+                        'serverResponse': page.status_code
+                    }
+                )
+                csvFile.flush()
+
+                # 获取页面url
+                # url_set_on_page = html_parser.get_url_set_on_page()
+                links = soup.find_all('a')
+
+                # 返回url默认值
+                returnUrlFull = ''
+
+                for item in links:
+                    print('item in links ==href:,  %s:\n ' %
+                          (item.get('href')))
+
+                    if item.get('href'):
+                        returnUrlFull = returnUrl(item.get('href'))
+
+                    # 本站点
+                    if returnUrlFull.startswith(urlTarget):
+                        urlSetOnPage.add(returnUrlFull)
+
+                # 获取种子url
+                targetUrlSeedSetTmp = urlSetOnPage
+                # print(targetUrlSeedSetTmp)
+                # exit()
+
+                # 构建映射
+                for urlTmp in targetUrlSeedSetTmp:
+                    urlMapParent[urlTmp] = urlFull
+
+                # print(urlMapParent)
+                # exit()
+
+                # url_set_not_visit = url_set_not_visit | target_url_seed_set_tmp
+                urlSetNotVisit = urlSetNotVisit | targetUrlSeedSetTmp
+
+            # url_set_visited = url_set_visited | target_url_seed_set
+            urlSetVisited = urlSetVisited | targetUrlSeedSet
+            targetUrlSeedSet = urlSetNotVisit - urlSetVisited
+
+            # print(targetUrlSeedSet, '\n====== \n', urlSetNotVisit,  '\n======\n', urlSetVisited)
             # exit()
-
-            # urlPath 转 完整url
-            # urlFull = returnUrl('https://www.cnipa.gov.cn')
-            urlFull = returnUrl(urlPath)
-            # print(urlPath, urlFull)
-            # exit()
-
-            # 下载页面
-            print('%s: 正在访问第 %s 个链接, urlFull:\n %s' %
-                  (time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()), pageVisitedNum, urlFull))
-            pageVisitedNum += 1
-
-            try:
-                page = getHttpRequest(urlFull)
-            except print(0):
-                pass
-            # page = getHttpRequest(urlFull)
-            # print(page.get("Content-Type"))
-            # exit()
-
-            htmlDoc = page.content.decode()
-            # 解析网页
-            soup = BeautifulSoup(htmlDoc, 'html.parser')
-            # print(htmlDoc)
-            # exit()
-
-            # 获取标题
-            # titles = soup.findall("<title>(.+?)</title>", str(htmlDoc))
-            # titles = soup.find_all('a')
-            titles = soup.find_all("title")
-            # links = soup.find_all('a')
-
-            # print(titles[0], titles[0].contents, titles[0].contents[0])
-            # exit()
-
-            if titles:
-                title = titles[0].contents[0]
-            else:
-                title = '页面标题为空'
-
-            # 写入 csv
-            pageWhereFound = ''
-            writer.writerow(
-                {
-                    'link': urlFull,
-                    'text': title,
-                    'pageWhereFound': str(urlMapParent.get(urlFull)),
-                    'serverResponse': page.status_code
-                }
-            )
-
-            # 获取页面url
-            # url_set_on_page = html_parser.get_url_set_on_page()
-            links = soup.find_all('a')
-
-            for item in links:
-                print('item in links ==href:,  %s:\n ' %
-                      (item.get('href')))
-                if item.get('href'):
-                    urlSetOnPage.add(returnUrl(item.get('href')))
-
-            # 获取种子url
-            targetUrlSeedSetTmp = urlSetOnPage
-            # print(targetUrlSeedSetTmp)
-            # exit()
-
-            # 构建映射
-            for urlTmp in targetUrlSeedSetTmp:
-                urlMapParent[urlTmp] = urlFull
-
-            # print(urlMapParent)
-            # exit()
-
-            # url_set_not_visit = url_set_not_visit | target_url_seed_set_tmp
-            urlSetNotVisit = urlSetNotVisit | targetUrlSeedSetTmp
-
-        # url_set_visited = url_set_visited | target_url_seed_set
-        urlSetVisited = urlSetVisited | targetUrlSeedSet
-        targetUrlSeedSet = urlSetNotVisit - urlSetVisited
-
-        print(targetUrlSeedSet, '\n====== \n', urlSetNotVisit,  '\n======\n', urlSetVisited)
-        # exit()
