4d42d9d95ab0eb779c2e967a939ef75e07813538
jieyanqiang <jieyanqiang@lf-network.com>
2022-12-03 19:57:38 +0800

    feat: input

--- url33.py
+++ url33.py
@@ -34,8 +34,9 @@ taskStartTime = time.time()
 # urlProtocol = 'http'
 # urlHost = 'www.adjyc.com'
 urlProtocol = input("目标站点协议,如 https:") or 'https'
+print('目标站点协议============' + urlProtocol)
 urlHost = input("目标站点url：如 www.cnipa.gov.cn:") or 'www.cnipa.gov.cn'
-
+print('目标站点url============' + urlHost)
 # 目标url
 urlTarget = urlProtocol + '://' + urlHost
 print('目标站点============' + urlTarget)
