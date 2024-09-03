f3ed734f60fa86c4f731be98c457e310d129e16f
揭衍强 <jieyanqiang@lf-network.com>
2024-09-03 20:44:17 +0800

    feat: design pattern callback

---
+++ design-pattern/callback/callback.py
@@ -0,0 +1,50 @@
+class Employee:
+    """公司员工"""
+
+    def __init__(self, name):
+        self.__name = name
+
+    def doPerformance(self, skill):
+        print(self.__name + "的表演:", end="")
+        skill()
+
+
+def sing():
+    """唱歌"""
+    print("唱一首歌")
+
+
+def dling():
+    """拉Ukulele"""
+    print("拉一曲Ukulele")
+
+
+def joke():
+    """说段子"""
+    print("说一搞笑段子")
+
+
+def performMagicTricks():
+    """表演魔术"""
+    print("神秘魔术")
+
+
+def skateboarding():
+    """玩滑板"""
+    print("酷炫滑板")
+
+
+def testSkill():
+    helen = Employee("Helen")
+    helen.doPerformance(sing)
+    frank = Employee("Frank")
+    frank.doPerformance(dling)
+    jacky = Employee("Jacky")
+    jacky.doPerformance(joke)
+    chork = Employee("Chork")
+    chork.doPerformance(performMagicTricks)
+    Kerry = Employee("Kerry")
+    Kerry.doPerformance(skateboarding)
+
+
+testSkill()
--- design-pattern/observer/observer_pattern.py
+++ design-pattern/observer/observer_pattern.py
@@ -1,29 +1,34 @@
 # print("1+1=", 1 + 1)
 # 观察者模式
 # 定义对象间的一种一对多的依赖关系，使得每当一个对象改变状态，则所有依赖于它的对象都会得到通知并自动更新。
-class Observer():
+class Observer:
     def update(self, message):
         raise NotImplementedError
+
 
 class ConcreteObserver(Observer):
     def __init__(self, name):
         self.name = name
-        
+
     def update(self, message):
         print("ConcreteObserver:", message)
 
-class Subject():
+
+class Subject:
     def __init__(self):
         self._observers = []
-        
+
     def add_observer(self, observer):
         self._observers.append(observer)
+
     def remove_observer(self, observer):
         self._observers.remove(observer)
+
     def notify_observers(self, message):
         for observer in self._observers:
             observer.update(message)
-    
+
+
 # 使用
 subject = Subject()
 observer1 = ConcreteObserver("observer1")
@@ -31,7 +36,3 @@ observer2 = ConcreteObserver("observer2")
 subject.add_observer(observer1)
 subject.add_observer(observer2)
 subject.notify_observers("hello world")
-      
-       
-				
-		
